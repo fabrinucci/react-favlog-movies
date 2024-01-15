336c24f3c067899958d5c7d7bde632d5504e2414
Fabrizio <fabrinucci99@gmail.com>
2024-01-15 18:04:48 -0300

    Update pagination

--- src/app/components/search/Pagination.tsx
+++ src/app/components/search/Pagination.tsx
@@ -8,7 +8,7 @@ interface Props {
   movies: Movies;
 }
 
-const createPaginationArray = (n: number) => {
+const createPagination = (n: number) => {
   let array = [];
   for (let i = 1; i <= n; i++) {
     array.push(i);
@@ -20,15 +20,50 @@ export const Pagination = ({ movies }: Props) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const query = searchParams.get('query');
+  const currentPage = movies.page;
+  const totalPages = movies.total_pages;
 
+  const LIMIT = 4;
+
+  const getStartingPage = () => {
+    if (currentPage <= Math.ceil(LIMIT / 2)) {
+      return 1;
+    } else if (currentPage >= totalPages - Math.ceil(LIMIT / 2)) {
+      return totalPages - 2;
+    } else {
+      return currentPage - Math.ceil(LIMIT / 2) + 1;
+    }
+  };
+
+  const getPaginationArray = () => {
+    const startingPage = getStartingPage();
+    const endingPage = Math.min(startingPage + LIMIT - 1, totalPages);
+    return createPagination(endingPage).slice(startingPage - 1);
+  };
   return (
     <div className='flex justify-center gap-6'>
-      {createPaginationArray(movies.total_pages).map((searchPage) => (
+      {currentPage > 1 && (
+        <>
+          <Link
+            href={`${pathname}?query=${query}&page=1`}
+            className='rounded-lg bg-gray-500 p-3 duration-200 ease-in-out hover:bg-violet-500'
+          >
+            {'<<<'}
+          </Link>
+          <Link
+            href={`${pathname}?query=${query}&page=${currentPage - 1}`}
+            className='rounded-lg bg-gray-500 p-3 duration-200 ease-in-out hover:bg-violet-500'
+          >
+            {'<'}
+          </Link>
+        </>
+      )}
+      {getPaginationArray().map((searchPage) => (
         <Link
           key={searchPage}
           href={`${pathname}?query=${query}&page=${searchPage}`}
           className={`rounded-lg p-3 ${
-            movies.page === searchPage
+            currentPage === searchPage
               ? 'bg-violet-600'
               : 'bg-gray-500 duration-200 ease-in-out hover:bg-violet-500'
           }`}
@@ -36,6 +71,23 @@ export const Pagination = ({ movies }: Props) => {
           {searchPage}
         </Link>
       ))}
+      {currentPage !== totalPages && (
+        <>
+          <Link
+            href={`${pathname}?query=${query}&page=${currentPage + 1}`}
+            className='rounded-lg bg-gray-500 p-3 duration-200 ease-in-out hover:bg-violet-500'
+          >
+            {'>'}
+          </Link>
+
+          <Link
+            href={`${pathname}?query=${query}&page=${totalPages}`}
+            className='rounded-lg bg-gray-500 p-3 duration-200 ease-in-out hover:bg-violet-500'
+          >
+            {'>>>'}
+          </Link>
+        </>
+      )}
     </div>
   );
 };
