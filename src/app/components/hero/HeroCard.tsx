fcc0e1aa83528590911463295ab46fefa7fa4ff6
Fabrizio <fabrinucci99@gmail.com>
2024-01-22 20:13:32 -0300

    Update Navbar and Hero

--- src/app/components/hero/HeroCard.tsx
+++ src/app/components/hero/HeroCard.tsx
@@ -33,43 +33,42 @@ export const HeroCard = ({ movie }: Props) => {
         />
       </picture>
 
-      <div className='absolute left-[50%] top-[60%] -translate-x-[50%] -translate-y-[50%] md:left-52 md:top-[40%] md:-translate-y-0 md:translate-x-0'>
-        <h1 className='mb-4 text-center text-2xl font-semibold sm:text-left sm:text-3xl md:mb-3'>
-          {movie.title}
-        </h1>
+      <section className='absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%]'>
+        <div className='flex flex-col items-center justify-center gap-6'>
+          <h1 className='text-center text-3xl font-semibold lg:text-4xl'>
+            {movie.title}
+          </h1>
 
-        <div className='flex flex-col items-center gap-3 sm:flex-row sm:items-center md:items-start'>
-          <p className='text-sm text-purple-300'>
-            Released: {movie.release_date}
-          </p>
-          <div className='flex gap-3'>
-            <p className='rounded-md bg-violet-600 px-2 py-[0.1rem] text-sm font-semibold'>
-              Trending
+          <div className='flex flex-col items-center gap-4 sm:flex-row'>
+            <p className='text-sm text-purple-300'>
+              Released: {movie.release_date}
             </p>
-            <div className='flex items-center justify-center gap-2'>
-              <div>
-                <GiRoundStar size={20} />
+            <div className='flex gap-3'>
+              <p className='rounded-md bg-violet-600 px-2 py-[0.1rem] text-sm font-semibold'>
+                Trending
+              </p>
+              <div className='flex items-center justify-center gap-2'>
+                <div>
+                  <GiRoundStar size={20} />
+                </div>
+                <p className='leading-4'>{movie.vote_average.toFixed(1)}</p>
               </div>
-              <p className='leading-4'>{movie.vote_average.toFixed(1)}</p>
             </div>
           </div>
-        </div>
 
-        <h2 className='mt-3 hidden text-sm leading-7 sm:w-[80%] sm:text-base md:block lg:w-[70%]'>
-          {movie.overview}
-        </h2>
-        <div className='mt-8 flex flex-col items-center gap-3 sm:flex-row md:mt-5 md:items-start'>
-          <button
-            onClick={handlePlay}
-            className='h-12 w-36 rounded-md border border-violet-600 bg-violet-600 px-5 py-2 duration-200 ease-in-out hover:bg-violet-700'
-          >
-            Play
-          </button>
-          <button className='h-12 w-36 rounded-md border border-violet-600 bg-[#0000006b] px-5 py-2 duration-200 ease-in-out hover:bg-[#000000cc]'>
-            Watch Later
-          </button>
+          <div className='flex flex-col items-center gap-4 sm:flex-row md:items-start'>
+            <button
+              onClick={handlePlay}
+              className='h-12 w-36 rounded-md border border-violet-600 bg-violet-600 px-5 py-2 duration-200 ease-in-out hover:bg-violet-700'
+            >
+              Play
+            </button>
+            <button className='h-12 w-36 rounded-md border border-violet-600 bg-[#0000006b] px-5 py-2 duration-200 ease-in-out hover:bg-[#000000cc]'>
+              Watch Later
+            </button>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,19 +1,20 @@
 import Link from 'next/link';
-import { MdCameraRoll } from 'react-icons/md';
+import { BsCameraReelsFill } from 'react-icons/bs';
 import { Account, Search } from './';
 
 export const Navbar = () => {
   return (
     <nav className='absolute z-[100] flex w-full flex-col items-center justify-between gap-4 bg-transparent p-4 sm:flex-row sm:gap-0 md:px-8'>
-      <Link href='/' className='flex items-center gap-2'>
-        <h2 className='text-xl font-semibold text-white'>Favlog</h2>
-        <MdCameraRoll color='white' />
-      </Link>
-
-      <div className='flex w-full items-center justify-evenly gap-3 sm:w-auto'>
-        <Search />
+      <div className='flex w-full items-center justify-around gap-3 sm:w-auto'>
+        <Link href='/' className='flex items-center gap-2'>
+          <h2 className='text-xl font-semibold text-white'>Favlog</h2>
+          <BsCameraReelsFill size={18} className='text-violet-200' />
+        </Link>
         <Account />
       </div>
+
+      <Search />
+
       <div className='hidden gap-2 sm:flex'>
         <button className='rounded-md border border-violet-600 bg-[#0000006b] px-5 py-2 text-white duration-200 ease-in-out hover:bg-[#000000b1]'>
           Sign In
