942bb03b5cc1ccd9141d357986f5bbb1aca57baa
Fabrizio <fabrinucci99@gmail.com>
2024-09-24 21:23:15 -0300

    delete: Remove getRandomImage

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -7,10 +7,6 @@ import type {
   MoviesResult,
 } from '@/interfaces';
 
-export const getRandomImage = (movies: MoviesResult[]) => {
-  return Math.floor(Math.random() * movies?.length);
-};
-
 export const getYear = (movieRelease: string) => {
   return movieRelease.split('-')[0];
 };
