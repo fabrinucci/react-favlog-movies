07129aeead3b7efd4e6559c7c2ae5f94e87a6a12
Fabrizio <fabrinucci99@gmail.com>
2024-10-01 17:46:16 -0300

    test: Update PersonalInfoCard test

--- src/app/components/person/__tests__/PersonalInfoCard.spec.tsx
+++ src/app/components/person/__tests__/PersonalInfoCard.spec.tsx
@@ -1,11 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import { PersonalInfoCard } from '@/components/person';
+import { getPerson } from '@/lib';
+import { mockedPerson } from '@/mocks/mockedResponse';
+
+jest.mock('../../../lib', () => ({
+  getPerson: jest.fn(),
+}));
 
 describe('Testing PersonalInfoCard', () => {
   it('Should be in the document', async () => {
+    (getPerson as jest.Mock).mockResolvedValue(mockedPerson);
     render(await PersonalInfoCard({ id: '105' }));
 
-    const title = screen.getByText('Personal Info');
-    expect(title).toBeInTheDocument();
+    const name = screen.getByText('Person name');
+    expect(name).toBeInTheDocument();
   });
 });
