39a3aaecc5a7f8d8c014ccdf7362eb9aadffdc54
Fabrizio <fabrinucci99@gmail.com>
2024-10-17 13:29:24 -0300

    test: Update CategoriesMovies test

--- src/app/components/category/__tests__/CategoriesMovies.spec.tsx
+++ src/app/components/category/__tests__/CategoriesMovies.spec.tsx
@@ -1,23 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import { CategoriesMovies } from '../CategoriesMovies';
 import { mockedPopularMovies } from '@/mocks/mockedResponse';
+import { generateCategories } from '@/mocks/mockers';
 
 describe('Testing CategoriesMovies', () => {
-  it('Should be in the component', () => {
+  test('Title should be the correct values', () => {
+    const mockedCategories = generateCategories(5);
     render(
       <CategoriesMovies
-        id='5'
-        categories={[
-          {
-            id: 2,
-            name: 'Horror',
-          },
-        ]}
+        id={`${mockedCategories[1].id}`}
+        categories={mockedCategories}
         movies={mockedPopularMovies}
       />
     );
 
-    const title = screen.getByText('Title');
+    const title = screen.getByTestId('category-title');
+
     expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent(mockedCategories[1].name);
+  });
+
+  test('Category list should be in the document', () => {
+    const mockedCategories = generateCategories(3);
+    render(
+      <CategoriesMovies
+        id='564'
+        categories={mockedCategories}
+        movies={mockedPopularMovies}
+      />
+    );
+
+    const categoryList = screen.getByTestId('category-list');
+    expect(categoryList).toBeInTheDocument();
   });
 });
