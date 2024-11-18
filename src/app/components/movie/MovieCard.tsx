a18be29485d9297b9c881d5697cb7a330ca87334
Fabrizio <fabrinucci99@gmail.com>
2024-11-18 13:29:14 -0300

    test: Update MovieCard tests

--- src/app/components/movie/MovieCard.tsx
+++ src/app/components/movie/MovieCard.tsx
@@ -24,6 +24,7 @@ export async function MovieCard({ id }: Props) {
       {movie.backdrop_path ? (
         <figure>
           <Image
+            data-testid='MovieCard-img'
             className='h-[120px] w-full object-cover sm:h-[80px]'
             src={`${MOVIE_PATH_LARGE}${movie.backdrop_path}`}
             alt={movie.title}
@@ -32,21 +33,35 @@ export async function MovieCard({ id }: Props) {
           />
         </figure>
       ) : (
-        <div className='h-[120px] sm:h-[80px]'></div>
+        <div
+          data-testid='MovieCard-img-default'
+          className='h-[120px] sm:h-[80px]'
+        ></div>
       )}
 
       <div className='p-6 md:p-8'>
         <MovieInfo movie={movie} movieCrew={movieCrew} />
 
         {movieCast.length === 0 ? (
-          <section className='mt-6'>
-            <h3 className='text-xl font-semibold'>No cast available</h3>
+          <section data-testid='MovieCard-no-cast' className='mt-6'>
+            <h3
+              data-testid='MovieCard-cast-title'
+              className='text-xl font-semibold'
+            >
+              No cast available
+            </h3>
           </section>
         ) : (
-          <section className='mt-6'>
+          <section data-testid='MovieCard-cast' className='mt-6'>
             <div className='flex items-center justify-between pb-3'>
-              <h3 className='text-xl font-semibold'>Cast:</h3>
+              <h3
+                data-testid='MovieCard-cast-title'
+                className='text-xl font-semibold'
+              >
+                Cast:
+              </h3>
               <Link
+                data-testid='MovieCard-cast-link'
                 href={`/movie/${movie.id}-${movieTitle}/cast`}
                 className='rounded-md border border-violet-600 bg-violet-600 px-4 py-3 font-semibold text-white duration-200 ease-in-out md:hover:scale-110'
               >
