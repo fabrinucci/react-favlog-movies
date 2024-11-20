89fe5321a72b1f19a9c614c5bb43802c9be7765a
Fabrizio <fabrinucci99@gmail.com>
2024-11-20 12:49:23 -0300

    test: Update SmallMovieCast tests

--- src/app/components/movie/SmallMovieCast.tsx
+++ src/app/components/movie/SmallMovieCast.tsx
@@ -14,19 +14,25 @@ export async function SmallMovieCast({ movieId }: Props) {
   const { MOVIE_PATH_SMALL, FEMALE_NOT_FOUND, MALE_NOT_FOUND } = config;
 
   return (
-    <ul className='grid grid-cols-fit-3 gap-x-6 gap-y-4'>
+    <ul
+      data-testid='SmallMovieCast'
+      className='grid grid-cols-fit-3 gap-x-6 gap-y-4'
+    >
       {movieCast.map((profileCast) => (
         <li
+          data-testid='SmallMovieCast-li'
           className='flex animate-fadeIn flex-col items-center justify-center'
           key={profileCast.id}
         >
           <Link
+            data-testid='SmallMovieCast-img-link'
             href={`/person/${profileCast.id}-${transformToSlug(
               profileCast.name
             )}`}
           >
             <figure className='my-2 h-60 w-40 rounded-md bg-purple-400'>
               <Image
+                data-testid='SmallMovieCast-img'
                 className='h-60 w-40 rounded-md object-cover'
                 src={
                   profileCast.profile_path
@@ -42,17 +48,24 @@ export async function SmallMovieCast({ movieId }: Props) {
             </figure>
           </Link>
           <Link
+            data-testid='SmallMovieCast-name-link'
             href={`/person/${profileCast.id}-${transformToSlug(
               profileCast.name
             )}`}
           >
-            <h4 className='text-center font-semibold transition-colors md:hover:text-violet-300'>
+            <h4
+              data-testid='SmallMovieCast-name'
+              className='text-center font-semibold transition-colors md:hover:text-violet-300'
+            >
               {profileCast.name?.length! <= 22
                 ? profileCast.name
                 : `${profileCast.name?.slice(0, 22)}...`}
             </h4>
           </Link>
-          <h5 className='text-sm text-purple-200'>
+          <h5
+            data-testid='SmallMovieCast-character'
+            className='text-sm text-purple-200'
+          >
             {profileCast.character?.length! <= 22
               ? profileCast.character
               : `${profileCast.character?.slice(0, 22)}...`}
--- src/app/components/movie/__tests__/SmallMovieCast.spec.tsx
+++ src/app/components/movie/__tests__/SmallMovieCast.spec.tsx
@@ -1,19 +1,176 @@
 import { render, screen } from '@testing-library/react';
 import { SmallMovieCast } from '../SmallMovieCast';
 import { getMovieCast } from '@/lib';
-import { mockedMovieCast } from '@/mocks/mockedResponse';
+import { generateMockedMovieCreditsCast } from '@/mocks/mockers';
+import { transformToSlug } from '@/utils';
+import { config } from '@/config';
+
+const { MOVIE_PATH_SMALL, FEMALE_NOT_FOUND, MALE_NOT_FOUND } = config;
 
 jest.mock('../../../lib', () => ({
   getMovieCast: jest.fn(),
 }));
 
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props: any) => {
+    return <img {...props} fill='true' />;
+  },
+}));
+
 describe('Testing <SmallMovieCast />', () => {
-  it('Should be in the component', async () => {
-    (getMovieCast as jest.Mock).mockResolvedValue(mockedMovieCast);
+  const mockedMovieCast = generateMockedMovieCreditsCast(10);
 
-    render(await SmallMovieCast({ movieId: '5' }));
+  describe('Test general', () => {
+    test('Should be in the component', async () => {
+      (getMovieCast as jest.Mock).mockResolvedValue(mockedMovieCast);
 
-    const title = screen.getByText('Movie Title');
-    expect(title).toBeInTheDocument();
+      render(await SmallMovieCast({ movieId: '5' }));
+
+      const list = screen.getByTestId('SmallMovieCast');
+      const listItems = await screen.findAllByTestId('SmallMovieCast-li');
+
+      expect(list).toBeInTheDocument();
+      expect(listItems).toHaveLength(10);
+    });
+  });
+
+  describe('Test images', () => {
+    test('Should have the correct src', async () => {
+      (getMovieCast as jest.Mock).mockResolvedValue(mockedMovieCast);
+
+      render(await SmallMovieCast({ movieId: '5' }));
+
+      const images = screen.getAllByTestId('SmallMovieCast-img');
+
+      expect(images[2]).toHaveAttribute(
+        'src',
+        `${MOVIE_PATH_SMALL}${mockedMovieCast[2].profile_path}`
+      );
+
+      expect(images[4]).toHaveAttribute(
+        'src',
+        `${MOVIE_PATH_SMALL}${mockedMovieCast[4].profile_path}`
+      );
+    });
+
+    test('Should have the correct src when there is no path', async () => {
+      mockedMovieCast[1].profile_path = '';
+      mockedMovieCast[1].gender = 1;
+
+      mockedMovieCast[3].profile_path = '';
+      mockedMovieCast[3].gender = 2;
+
+      mockedMovieCast[5].profile_path = '';
+      mockedMovieCast[5].gender = 3;
+
+      mockedMovieCast[7].profile_path = '';
+      mockedMovieCast[7].gender = 0;
+
+      (getMovieCast as jest.Mock).mockResolvedValue(mockedMovieCast);
+
+      render(await SmallMovieCast({ movieId: '5' }));
+
+      const images = screen.getAllByTestId('SmallMovieCast-img');
+
+      expect(images[1]).toHaveAttribute('src', FEMALE_NOT_FOUND);
+      expect(images[3]).toHaveAttribute('src', MALE_NOT_FOUND);
+      expect(images[5]).toHaveAttribute('src', MALE_NOT_FOUND);
+      expect(images[7]).toHaveAttribute('src', MALE_NOT_FOUND);
+    });
+
+    test('Should have the alt text', async () => {
+      (getMovieCast as jest.Mock).mockResolvedValue(mockedMovieCast);
+
+      render(await SmallMovieCast({ movieId: '5' }));
+
+      const images = screen.getAllByTestId('SmallMovieCast-img');
+
+      expect(images[1]).toHaveAttribute('alt', mockedMovieCast[1].name);
+      expect(images[3]).toHaveAttribute('alt', mockedMovieCast[3].name);
+      expect(images[5]).toHaveAttribute('alt', mockedMovieCast[5].name);
+    });
+  });
+
+  describe('Test links', () => {
+    test('Image should have the correct link', async () => {
+      (getMovieCast as jest.Mock).mockResolvedValue(mockedMovieCast);
+
+      render(await SmallMovieCast({ movieId: '5' }));
+      const imgLinks = screen.getAllByTestId('SmallMovieCast-img-link');
+
+      expect(imgLinks[1]).toHaveAttribute(
+        'href',
+        `/person/${mockedMovieCast[1].id}-${transformToSlug(
+          mockedMovieCast[1].name
+        )}`
+      );
+
+      expect(imgLinks[3]).toHaveAttribute(
+        'href',
+        `/person/${mockedMovieCast[3].id}-${transformToSlug(
+          mockedMovieCast[3].name
+        )}`
+      );
+    });
+
+    test('Name should have the correct link', async () => {
+      (getMovieCast as jest.Mock).mockResolvedValue(mockedMovieCast);
+
+      render(await SmallMovieCast({ movieId: '5' }));
+
+      const nameLinks = screen.getAllByTestId('SmallMovieCast-name-link');
+
+      expect(nameLinks[1]).toHaveAttribute(
+        'href',
+        `/person/${mockedMovieCast[1].id}-${transformToSlug(
+          mockedMovieCast[1].name
+        )}`
+      );
+
+      expect(nameLinks[3]).toHaveAttribute(
+        'href',
+        `/person/${mockedMovieCast[3].id}-${transformToSlug(
+          mockedMovieCast[3].name
+        )}`
+      );
+    });
+
+    test('Links should have an accessible name', async () => {
+      (getMovieCast as jest.Mock).mockResolvedValue(mockedMovieCast);
+
+      render(await SmallMovieCast({ movieId: '5' }));
+      const imgLinks = screen.getAllByTestId('SmallMovieCast-img-link');
+      const nameLinks = screen.getAllByTestId('SmallMovieCast-name-link');
+
+      expect(imgLinks[1]).toHaveAccessibleName(mockedMovieCast[1].name);
+      expect(imgLinks[3]).toHaveAccessibleName(mockedMovieCast[3].name);
+      expect(imgLinks[7]).toHaveAccessibleName(mockedMovieCast[7].name);
+
+      expect(nameLinks[2]).toHaveAccessibleName(mockedMovieCast[2].name);
+      expect(nameLinks[4]).toHaveAccessibleName(mockedMovieCast[4].name);
+      expect(nameLinks[6]).toHaveAccessibleName(mockedMovieCast[6].name);
+    });
+  });
+
+  describe('Test name and character', () => {
+    test('Should have the "..." when exceeding 22 characters.', async () => {
+      mockedMovieCast[0].name = 'Carl James';
+      mockedMovieCast[1].name = 'VeryLongActorNameExample';
+
+      mockedMovieCast[4].character = 'VeryLongCharacterNameExample';
+
+      (getMovieCast as jest.Mock).mockResolvedValue(mockedMovieCast);
+
+      render(await SmallMovieCast({ movieId: '5' }));
+      const names = screen.getAllByTestId('SmallMovieCast-name');
+      const characters = screen.getAllByTestId('SmallMovieCast-character');
+
+      expect(names[0]).toHaveTextContent('Carl James');
+      expect(names[1]).toHaveTextContent('VeryLongActorNameExamp...');
+
+      expect(characters[3]).toHaveTextContent(mockedMovieCast[3].character);
+      expect(characters[4]).toHaveTextContent('VeryLongCharacterNameE...');
+    });
   });
 });
