e74f6b82e967d41a7f0a92046d3ffc1cd81443fe
Fabrizio <fabrinucci99@gmail.com>
2024-11-25 21:57:49 -0300

    test: Update Navbar tests

--- src/app/components/navbar/__tests__/Navbar.spec.tsx
+++ src/app/components/navbar/__tests__/Navbar.spec.tsx
@@ -2,18 +2,53 @@ import { render, screen } from '@testing-library/react';
 import { Navbar } from '../Navbar';
 
 jest.mock('../Account', () => ({
-  Account: () => <div data-testid='account'></div>,
+  Account: () => <div data-testid='Account'></div>,
 }));
 
 jest.mock('../Search', () => ({
-  Search: () => <div data-testid='search'></div>,
+  Search: () => <div data-testid='Search'></div>,
 }));
 
-describe('Testing Search', () => {
-  it('Should be in the document', () => {
+describe('Testing Navbar', () => {
+  test('Should be in the document', () => {
     render(<Navbar />);
 
-    const button = screen.getByText('Sign In');
-    expect(button).toBeInTheDocument();
+    const navbar = screen.queryByRole('navigation');
+    const search = screen.queryByTestId('Search');
+    const account = screen.queryByTestId('Account');
+
+    const homeLink = screen.queryByTestId('Navbar-home-link');
+    const buttons = screen.queryByTestId('Navbar-btn');
+    const signIn = screen.queryByTestId('Navbar-btn-sign-in');
+    const signUp = screen.queryByTestId('Navbar-btn-sign-up');
+
+    expect(navbar).toBeInTheDocument();
+    expect(account).toBeInTheDocument();
+    expect(search).toBeInTheDocument();
+
+    expect(homeLink).toBeInTheDocument();
+    expect(buttons).toBeInTheDocument();
+    expect(signIn).toBeInTheDocument();
+    expect(signUp).toBeInTheDocument();
+  });
+
+  test('Should have the correct link values', () => {
+    render(<Navbar />);
+
+    const homeLink = screen.queryByTestId('Navbar-home-link');
+
+    expect(homeLink).toHaveAttribute('href', '/');
+    expect(homeLink).toHaveTextContent('Favlog');
+    expect(homeLink).toHaveAccessibleName('Go home');
+  });
+
+  test('Buttons should have the correct text', () => {
+    render(<Navbar />);
+
+    const signIn = screen.queryByTestId('Navbar-btn-sign-in');
+    const signUp = screen.queryByTestId('Navbar-btn-sign-up');
+
+    expect(signIn).toHaveTextContent('Sign In');
+    expect(signUp).toHaveTextContent('Sign Up');
   });
 });
